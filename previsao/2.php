2b9b585c0696d7a543f45339acfc91aca4011364
Estevanzc <esteveplays@gmail.com>
2024-06-21 12:17:27 -0300

    previsao (ainda_nao)

--- previsao/2.php
+++
@@ -1,6 +0,0 @@
-<?php
-$curl = curl_init("http://servicos.cptec.inpe.br/XML/listaCidades?city=sao%20paulo");
-curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-$return = curl_exec($curl);
-$return = simplexml_load_string($return);
-print_r($return);
